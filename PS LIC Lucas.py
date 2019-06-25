0445846e711e2a8cb031252924097145ba67751a
Lucas <lucaspinho101@gmail.com>
2019-06-25 13:58:12 -0300

    Create PS LIC Lucas.py

---
+++ PS LIC Lucas.py
@@ -0,0 +1,37 @@
+#Exercicio 1
+def resumir(texto):
+    tamanho = len(texto)
+    if (tamanho < 140):
+
+
+
+
+#Exercicio 2
+def numero(num):
+    tamanho = len(num)
+    if tamanho == 10:
+        print(num[0],',',num[1:3],'Bilhões')
+    elif tamanho == 9:
+        print(num[0;3,'Milhões')
+    else tamanho == 11:
+              print(num[0:2],',',num[2],'Bilhões')
+
+  
+
+
+
+#Exercício 3
+fimes = {'2013': ['Frozen 1276480335'],
+         '2012': ['Marvels The Avengers 1518812988'],
+         '1997': ['Titanic 2187463944'],
+         '2018': ['Black Panther 1346913161','Avengers: Infinity War 2048359754','Jurassic World: Fallen Kingdom 1309484461']
+         '2015': ['Avengers: Age Of Ultron 1405403694','Star Wars: The Force Awakens 2068223624']
+         '2009': ['Avatar 2787965087']
+         '2017': ['Star Wars: The Last Jedi 1332539889', 'Beauty and the Beast 1263521126']
+         '2019': ['Avengers: Endgame 2750667499']
+         '2011': ['Harry Potter and the Deathly Hallows – Part 2 1341693157']
+         }
+         
+         
+
+
